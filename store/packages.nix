037c51cb1001235a9162677b2bbca180fd625d50
F. ICE <fyhb233@gmail.com>
2025-03-14 00:01:12 +0800

    [25-03-14] add fastfetch

--- store/packages.nix
+++ store/packages.nix
@@ -1,6 +1,6 @@
 { pkgs, ... } :
 {
   home.packages = with pkgs; [
-    nh lazygit lazydocker gradience nix-init nil nixfmt-rfc-style eza fzf ripgrep fd bat gnupg dust
+    nh lazygit lazydocker gradience nix-init nil nixfmt-rfc-style eza fzf ripgrep fd bat gnupg dust fastfetch
   ];
 }
